c7ae2b7a80296de5765fdd39a517c14a15223e09
Jonathas Lacerda <jonathassl@gmail.com>
2017-02-18 12:23:06 -0300

    Modelagem inicial

---
+++ src/altaqias/ragatanga/model/Cliente.java
@@ -0,0 +1,35 @@
+package altaqias.ragatanga.model;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Cliente {
+
+	@Getter @Setter
+	private Integer id;
+	
+	@Getter @Setter
+	private Integer documento;
+	
+	@Getter @Setter
+	private String nome;
+	
+	@Getter @Setter
+	private String email;
+	
+	@Getter @Setter
+	private String telefone;
+	
+	@Getter @Setter
+	private Pais nacionalidade;
+	
+	@Getter @Setter
+	private Date dataNascimento;
+	
+	@Getter @Setter
+	private List<Idioma> idiomas;
+	
+}
