9ee4f4b537fb0e57d1d51385547b1797409d3edf
Jonathas Lacerda <jonathassl@gmail.com>
2017-02-18 15:09:25 -0300

    opa

--- src/altaqias/ragatanga/model/Quest.java
+++ src/altaqias/ragatanga/model/Quest.java
@@ -1,5 +1,7 @@
 package altaqias.ragatanga.model;
 
+import java.math.BigDecimal;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,5 +12,14 @@ public class Quest {
 	
 	@Getter @Setter
 	private Cliente criador;
+	
+	@Getter @Setter
+	private BigDecimal valor;
+	
+	@Getter @Setter
+	private Date prazo;
+	
+	@Getter @Setter
+	private Destino destino;
 	
 }
