d1b7d3101395b039326fb29f8ad27aee5351d025
jonny <jonny@BARBATOS>
2017-02-18 15:04:51 -0300

    controller cliente existe por documento existe por email

--- src/altaqias/ragatanga/api/query/ClienteQuery.java
+++ src/altaqias/ragatanga/api/query/ClienteQuery.java
@@ -2,6 +2,8 @@ package altaqias.ragatanga.api.query;
 
 public class ClienteQuery {
 
-	public static final String LOGIN = "SELECT c FROM Cliente c WHERE c.email = :email AND c.senha = :senha";
+	public static final String AUTENTICAR = "SELECT c FROM Cliente c WHERE c.email = :email AND c.senha = :senha";
+	public static final String EXISTE_POR_EMAIL = "SELECT c FROM Cliente c WHERE c.email = :email";
+	public static final String EXISTE_POR_DOCUMENTO = "SELECT c FROM Cliente c WHERE c.documento = :documento";
 	
 }
